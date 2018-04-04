dc477edbbb4e3707e27e82cba5e811ab689b7e7b
Sergi Mascaró <902382@edu.p.lodz.pl>
2018-04-04 17:58:56 +0200

    HQ service created

---
+++ WCF_SERVICE_HQ/HQService/HQService/HQService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HQService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class HQService : IHQService
+    {
+        public bool CheckInsertIsDone(string _name, string _type, int _quant, int _price, int _cost)
+        {
+            ProductEntity new_p = new ProductEntity();
+            new_p.Name = _name;
+            new_p.Type = _type;
+            new_p.Quantity = _quant;
+            new_p.Price = _price;
+            new_p.Cost = _cost;
+            using (var ctx = new ChocolateCoHQEntities())
+            {
+                var res = ctx.ProductEntities.Add(new_p);
+                ctx.SaveChanges();
+                return (res != null);
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ WCF_SERVICE_HQ/HQService/HQService/IHQService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HQService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IHQService
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        bool CheckInsertIsDone(string _name, string _type, int _quant, int _price, int _cost);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
