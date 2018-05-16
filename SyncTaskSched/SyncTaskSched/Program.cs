61f7f63222513b18f1c6bc82c8641a3c9fe04267
Sergi Mascaró <msk1416@gmail.com>
2018-05-16 19:27:40 +0200

    added synchronizer application

---
+++ SyncTaskSched/SyncTaskSched/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32.TaskScheduler;
+
+namespace SyncTaskSched
+{
+    class Program
+    {
+        private static String EXEPATH = "C:\\Users\\sergi\\Desktop\\Politechnika Lodzka\\INTERNET APPLICATIONS PROGRAMMING\\WCF_SOAP_REST\\git\\newtry\\chocolatefactory\\UpdateService\\UpdateService\\bin\\Release\\UpdateService.exe";
+        static void Main(string[] args)
+        {
+            using (TaskService ts = new TaskService())
+            {
+                TaskDefinition td = ts.NewTask();
+                td.RegistrationInfo.Description = "Updates product stock every 4 hours.";
+
+                var dtrig = new DailyTrigger();
+                dtrig.StartBoundary = DateTime.Now;
+                dtrig.Repetition.Interval = TimeSpan.FromHours(4);
+                td.Triggers.Add(dtrig);
+
+                td.Actions.Add(new ExecAction(EXEPATH));
+
+                ts.RootFolder.RegisterTaskDefinition("StockUpdate", td);
+            } 
+        }
+    }
+}
