ecc52b9d1ed25aa4f172afba621b80822fc22351
Piotr Michalski <piotr.km59@gmail.com>
2018-05-17 09:40:25 +0200

    Working Branch admin app
    accepting, declining, resources. Ima hacker. Ave Hungito

--- LocalAdminApp/LocalAdminApp/MainPage.xaml.cs
+++ LocalAdminApp/LocalAdminApp/MainPage.xaml.cs
@@ -45,7 +45,7 @@ namespace LocalAdminApp
             }
             try
             {
-                await client.acceptOrderAsync(PendingOrdersListView.SelectedIndex);
+                await client.acceptOrderAsync(pendingOrdersIdList[PendingOrdersListView.SelectedIndex]);
                 PendingOrdersTextBox.Text = "Pending order " + pendingOrdersIdList[PendingOrdersListView.SelectedIndex] + " accepted.";
             }
             catch (Exception exception)
@@ -86,10 +86,95 @@ namespace LocalAdminApp
             var prodArray = productsList.ToArray();
             foreach (PendingOrderDTO p in pendingOrders)
             {
-                PendingOrdersListView.Items.Add(p.OrderID + " Client " + clientsList[p.ProductID] + " product " + productsList[p.ProductID] + " quantity " + p.Quantity);
+                PendingOrdersListView.Items.Add(p.OrderID + " Client " + clientsList[p.ProductID] +
+                    " product " + productsList[p.ProductID] + " quantity " + p.Quantity);
             }
             await client.CloseAsync();
 
+        }
+
+        private async void DeclineOrderButton_Click(object sender, RoutedEventArgs e)
+        {   
+
+            if(PendingOrdersTextBox.Text != null)
+            { 
+                ProductServiceClient client
+                    = new ProductServiceClient();
+                var pendingOrders = await client.getPendingOrdersAsync();
+
+                List<int> pendingOrdersIdList = new List<int>();
+                pendingOrdersIdList.Clear();
+
+                foreach (PendingOrderDTO po in pendingOrders)
+                {
+                    pendingOrdersIdList.Add(po.OrderID);
+                }
+                try
+                {
+                    
+                    await client.dismissOrderAsync(pendingOrdersIdList[PendingOrdersListView.SelectedIndex], PendingOrdersTextBox.Text);
+                    PendingOrdersTextBox.Text = "Pending order " + pendingOrdersIdList[PendingOrdersListView.SelectedIndex] + " declined.";
+                }
+                catch (Exception exception)
+                {
+                    PendingOrdersTextBox.Text = "Pending order " + pendingOrdersIdList[PendingOrdersListView.SelectedIndex] + "not declined." + exception;
+                }
+            }
+            else
+            {
+                PendingOrdersTextBox.Text = "Insert justification";
+
+            }
+
+            await client.CloseAsync();
+        }
+
+        private async void RefreshResourceButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResourcesListView.Items.Clear();
+            ProductServiceClient client
+                = new ProductServiceClient();
+            
+            var products = await client.getProductsAsync();
+            
+            foreach (ProductDTO pr in products)
+            {
+                ResourcesListView.Items.Add(pr.ProductID + " Product name: " + pr.ProductName + "   Quantity: " + pr.Quantity);
+            }
+            
+            await client.CloseAsync();
+
+        }
+
+        private async void OrderResourceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ResourcesTextBox.Text != null)
+            {
+                ProductServiceClient client
+                    = new ProductServiceClient();
+                var resources = await client.getProductsAsync();
+                List<int> productsIdList = new List<int>();
+                productsIdList.Clear();
+                foreach (ProductDTO pr in resources)
+                {
+                    productsIdList.Add(pr.ProductID);
+                }
+                try
+                {
+                    await client.requestStockToHQAsync(productsIdList[ResourcesListView.SelectedIndex], int.Parse(ResourcesTextBox.Text));
+                    ResourcesTextBox.Text = "Resource " + productsIdList[ResourcesListView.SelectedIndex] + " ordered.";
+                }
+                catch (Exception exception)
+                {
+                    ResourcesTextBox.Text = "Resource " + productsIdList[ResourcesListView.SelectedIndex] + " not ordered." + exception;
+                }
+
+            }
+            else
+            {
+                ResourcesTextBox.Text = "Insert ordered value";
+            }
+            
         }
     }
 }
